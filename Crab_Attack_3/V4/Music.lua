486582881fe4514cf35c89392e0de62dbe9ffa00
AndrewWood99 <54746062+AndrewWood99@users.noreply.github.com>
2025-05-17 13:47:09 -0400

    Add files via upload

---
+++ Crab_Attack_3/V4/Music.lua
@@ -0,0 +1,19 @@
+Music = Class{}
+
+function Music:init()
+	-- initialize our table of sounds
+    sounds = {
+        -- https://www.newgrounds.com/audio/listen/661012
+        ['music'] = love.audio.newSource('Space-Battle.mp3', 'static')
+    }
+
+    -- Set Looping True
+    sounds['music']:setLooping(true)
+
+    
+end
+
+function Music:update(track, volume)
+	sounds[track]:setVolume(0.1)
+    sounds[track]:play()
+end
